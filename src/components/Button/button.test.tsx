301d6b80b731072f42e211f1b6cac894463d5628
guojinliang <496168739@qq.com>
2021-08-16 16:24:38 +0800

    refactor button component

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,10 +1,60 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import Button from "./button";
+import { render, fireEvent } from "@testing-library/react";
+import Button, { ButtonProps } from "./button";
+const defaultProps: ButtonProps = {
+  onClick: jest.fn(),
+};
 
-test("our first react test case", () => {
-  const wrapper = render(<Button>Nice</Button>);
-  const element = wrapper.queryByText("Nice");
-  expect(element).toBeTruthy();
-  expect(element).toBeInTheDocument();
+const textProps: ButtonProps = {
+  className: "test",
+  btnType: "primary",
+  size: "lg",
+};
+
+const disabledProps = {
+  onClick: jest.fn(),
+  disabled: true,
+};
+
+describe("test button component", () => {
+  test("should render the correct default button", () => {
+    const wrapper = render(<Button {...defaultProps}>default button</Button>);
+    const element = wrapper.getByText("default button") as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("BUTTON");
+    expect(element).toHaveClass("btn btn-default");
+    expect(element.disabled).toBeFalsy();
+    fireEvent.click(element);
+    expect(defaultProps.onClick).toHaveBeenCalled();
+  });
+  it("should render the correct component based on different props", () => {
+    const wrapper = render(
+      <Button {...textProps}>primary button with class</Button>
+    );
+    const element = wrapper.getByText("primary button with class");
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("BUTTON");
+    expect(element).toHaveClass("btn test btn-primary btn-lg");
+  });
+  it("should render a link when btnType equals link and href is provided", () => {
+    const wrapper = render(
+      <Button btnType="link" href="//www.baidu.com">
+        link
+      </Button>
+    );
+    const element = wrapper.getByText("link");
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("A");
+    expect(element).toHaveClass("btn btn-link");
+  });
+  it("should render disabled button when disabled set to true", () => {
+    const wrapper = render(<Button {...disabledProps}>disabled button</Button>);
+    const element = wrapper.getByText("disabled button") as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("BUTTON");
+    expect(element).toHaveAttribute("disabled");
+    expect(element.disabled).toBeTruthy();
+    fireEvent.click(element);
+    expect(disabledProps.onClick).not.toHaveBeenCalled();
+  });
 });
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,17 +1,8 @@
 import React from "react";
 import classnames from "classnames";
 
-export enum ButtonSize {
-  Large = "lg",
-  Small = "sm",
-}
-
-export enum ButtonType {
-  Primary = "primary",
-  Default = "default",
-  Danger = "danger",
-  Link = "link",
-}
+export type ButtonSize = "lg" | "small";
+export type ButtonType = "primary" | "default" | "danger" | "link";
 
 interface BaseButtonProps {
   className?: string;
@@ -30,23 +21,16 @@ type AnchorButtonProps = BaseButtonProps &
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
 const Button: React.FC<ButtonProps> = (props) => {
-  const {
-    btnType,
-    disabled,
-    size,
-    href,
-    children,
-    className,
-    ...restProps
-  } = props;
+  const { btnType, disabled, size, href, children, className, ...restProps } =
+    props;
 
   const classes = classnames("btn", className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    disabled: btnType === ButtonType.Link && disabled,
+    disabled: btnType === "link" && disabled,
   });
 
-  if (btnType === ButtonType.Link && href) {
+  if (btnType === "link" && href) {
     return (
       <a className={classes} href={href} {...restProps}>
         {children}
@@ -63,7 +47,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default,
+  btnType: "default",
 };
 
 export default Button;
