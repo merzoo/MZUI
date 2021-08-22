6e21f18b5b6ee708f496ed5c6e9b02151a5d425a
guojinliang <496168739@qq.com>
2021-08-22 18:54:07 +0800

    chore: config sass-loader

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,2 +1,3 @@
 import { configure, addDecorator } from "@storybook/react";
+import "../src/styles/index.scss";
 configure(require.context("../src", true, /\.stories\.tsx$/), module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,27 @@
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    use: [
-      {
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: [require.resolve("babel-preset-react-app")],
+  config.module.rules.push(
+    {
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [require.resolve("babel-preset-react-app")],
+          },
         },
-      },
-    ],
-  });
+      ],
+    },
+    {
+      test: /\.scss?$/,
+      use: [
+        { loader: require.resolve("style-loader") },
+        { loader: require.resolve("css-loader") },
+        {
+          loader: require.resolve("sass-loader"),
+        },
+      ],
+    }
+  );
   config.resolve.extensions.push(".ts", ".tsx");
 
   return config;
